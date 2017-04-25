dc4660a5b321b35f478c7473f3e15429e02cf6d0
davidroost <davidroost@David-Roosts-MacBook-Pro.local>
2017-04-25 10:39:30 -0700

    upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $("nav ul li:first-child").click(function(){
+      console.log("hamburger menu clicked!");
+      // $("ul li a").slideToggle();
+      $("nav").toggleClass("open");
+  });
+
+  /*
+
+  toggle()
+  slideDown()
+
+  */
+});

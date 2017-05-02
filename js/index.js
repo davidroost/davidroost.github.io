9f153ec2c66977702a0c5e444023efa15cb7d674
davidroost <davidroost@David-Roosts-MacBook-Pro.local>
2017-05-01 22:58:52 -0700

    more photos

--- js/index.js
+++ js/index.js
@@ -103,6 +103,19 @@ $(document).ready(function(){
 
 // publication viewer
 
+
+// slideshow
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
 });
 
 
